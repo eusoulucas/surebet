4ab0c23c5c9c35a62227ae424173a9857101966b
eusoulucas_personal <lucadito@gmail.com>
2023-01-23 14:21:41 -0300

    até aqui está pegando um jogo apenas

--- scratch.py
+++ scratch.py
@@ -15,3 +15,42 @@ wait = WebDriverWait(driver, 10)
 # Closing the popup as soon as it shows
 close_popup = wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/section[2]/div[7]/div/div/div[1]/button')))
 close_popup.click()
+
+elements = driver.find_elements(By.XPATH, '/html/body/div[1]/div/section[2]/div[5]/div[2]/section/div[4]/div/div[1]/div[2]/table')
+
+# Extract the text of the elements
+infos = [element.text for element in elements]
+
+# Print the infos
+for info in infos:
+    aux = info.split('\n')
+    print(aux)
+
+data = {
+    "data": aux[0],
+    "horario": aux[1],
+    "time_casa": aux[2],
+    "time_visitante": aux[3],
+    str(aux[5]): {
+        aux[2]: aux[6],
+        'empate': aux[7],
+        aux[3]: aux[8]
+    },
+    str(aux[9]): {
+        aux[10]: aux[11],
+        aux[12]: aux[13]
+    },
+    str(aux[14]): {
+        aux[15]: aux[16],
+        aux[17]: aux[18]
+    },
+}
+
+
+import pandas as pd
+df = pd.json_normalize(data)
+#df = pd.DataFrame(data)
+print(df)
+
+# Close the browser
+driver.quit()
